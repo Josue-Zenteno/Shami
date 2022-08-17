957cc2c383aad3c5d34e3856dfa2f87fc8c79b0f
Josue WSL <dev.carlos.morales@gmail.com>
2022-08-16 19:15:05 -0500

    feat: add prettier and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import * as path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,8 +14,7 @@ export default defineConfig({
       { find: 'Utils', replacement: path.resolve(__dirname, 'src/utils') },
       { find: 'Data', replacement: path.resolve(__dirname, 'src/data') },
       { find: 'Pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: 'Redux', replacement: path.resolve(__dirname, 'src/redux') }
-    ]
-  }
-});
-
+      { find: 'Redux', replacement: path.resolve(__dirname, 'src/redux') },
+    ],
+  },
+})
